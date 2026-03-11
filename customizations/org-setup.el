10147ebdb9151281ce19ecb52f8047dc5c8d0f9a
Uziel Linares <>
2026-03-10 22:25:38 -0500

    add gitignore and keybindings for org files

--- customizations/org-setup.el
+++ customizations/org-setup.el
@@ -13,6 +13,8 @@
 (global-set-key (kbd "C-c l") #'org-store-link)
 (global-set-key (kbd "C-c a") #'org-agenda)
 (global-set-key (kbd "C-c c") #'org-capture)
+(global-set-key (kbd "C-c j") (lambda () (interactive) (find-file "~/org/journal.org")))
+(global-set-key (kbd "C-c o") (lambda () (interactive) (find-file "~/org/agenda.org")))
 
 (setq org-capture-templates
       '(("t" "TODO" entry (file+headline "~/org/agenda.org" "Inbox")
