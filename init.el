4335dc08269447e8960b49e4a74a27cb9b1b8011
Uziel Linares <>
2026-04-05 01:09:30 -0500

    clean up init file and use straight for packages

--- init.el
+++ init.el
@@ -1,23 +1,38 @@
 ;; My config
-
 (setq custom-file (locate-user-emacs-file "custom.el"))
 
+;; straight
+(defvar bootstrap-version)
+(let ((bootstrap-file
+       (expand-file-name
+        "straight/repos/straight.el/bootstrap.el"
+        (or (bound-and-true-p straight-base-dir)
+            user-emacs-directory)))
+      (bootstrap-version 7))
+  (unless (file-exists-p bootstrap-file)
+    (with-current-buffer
+        (url-retrieve-synchronously
+         "https://raw.githubusercontent.com/radian-software/straight.el/develop/install.el"
+         'silent 'inhibit-cookies)
+      (goto-char (point-max))
+      (eval-print-last-sexp)))
+  (load bootstrap-file nil 'nomessage))
+
 ;; Packages
-(require 'package)
-(add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/") t)
-
-(package-initialize)
-
-(require 'use-package-ensure)
-(setq use-package-always-ensure t)
+(straight-use-package 'use-package)
+(setq straight-use-package-by-default t)
 (use-package magit)
 (use-package zig-mode)
-(use-package company)
-(use-package org)
+(use-package company
+  :config
+  (add-hook 'after-init-hook 'global-company-mode))
+(use-package ivy
+  :config
+  (ivy-mode)
+  (setopt ivy-use-virtual-buffers t)
+  (setopt enable-recursive-minibuffers t))
 (use-package go-mode)
 (use-package rust-mode)
-(use-package ivy)
-(add-hook 'after-init-hook 'global-company-mode)
 (use-package vterm)
 (use-package yaml-mode)
 (use-package auctex)
@@ -27,13 +42,9 @@
 (use-package nov)
 (use-package elfeed)
 (use-package yasnippet
-  :ensure t
   :config
   (yas-reload-all)
   (add-hook 'prog-mode-hook #'yas-minor-mode))
-
-(when (not package-archive-contents)
-  (package-refresh-contents))
 
 ;; registers and bookmarks
 (set-register ?i '(file . "~/.config/emacs/init.el"))
@@ -42,11 +53,12 @@
 (setq bookmark-save-flag 1)
 
 (add-to-list 'load-path "~/.config/emacs/customizations")
-(load "custom-stuff.el")
-(load "ui.el")
-(load "programming.el")
-(load "email.el")
-(load "keybindings.el")
-(load "org-setup.el")
-(load "dired-setup.el")
-(load "elfeed-setup.el")
+(load "custom-stuff")
+(load "ui")
+(load "programming")
+(load "email")
+(load "keybindings")
+(load "org-setup")
+(load "dired-setup")
+(load "elfeed-setup")
+(load custom-file :no-error-if-file-is-missing)
